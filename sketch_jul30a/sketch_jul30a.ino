fd8692ff983ea1be3675c0232b5fbf4b1f6d2e08
dyingcarl <carluner@gmail.com>
2017-07-30 20:09:15 +0800

    WorkingBut..
    Now it's working and can take 3 ints then display certain color LED. But
    differently from the book, the program cannot detect ENTER button with
    '\r' apparently.

--- sketch_jul30a/sketch_jul30a.ino
+++ sketch_jul30a/sketch_jul30a.ino
@@ -20,40 +20,69 @@ void setup() {
 
 }
 
-/*void loop() {
+void loop() {
   // put your main code here, to run repeatedly:
 
   int inChar;
 
   if(Serial.available()>0){
     inChar = Serial.read();
+    Serial.println("\n\nsSerial availability verified.");
   }
 
   if(isDigit(inChar)){
     inString += (char)inChar;
+    Serial.println("\n\nInput detected, inString has been changed.");
   }
 
   if(inChar == ','){
+    Serial.println("\n\nSwitch on the way.");
     switch(currentColor){
       case 0:
         red = inString.toInt();
         inString = "";
+        Serial.println("\n\nRed parameter ready, inString cleared.");
+        analogWrite(10,255-red);
+        Serial.print(red);
+        Serial.print(green);
+        Serial.print(blue);
         break;
 
       case 1:
         green = inString.toInt();
         inString = "";
+        Serial.println("\n\nGreen parameter ready, inString cleared.");
+        analogWrite(9,255-green);
+        Serial.print(red);
+        Serial.print(green);
+        Serial.print(blue);
+        break;
+
+      case 2:
+        blue = inString.toInt();
+        inString = "";
+        Serial.println("\n\nBlue parameter ready, inString cleared.");
+        analogWrite(11,255-blue);
+        Serial.print(red);
+        Serial.print(green);
+        Serial.print(blue);
         break;
     }
 
     currentColor++;
+    if(currentColor == 3){
+      currentColor = 0;
+    }
   }
 
-  if(inChar == '\r'){
+  /*if(inChar == '\r'){
+
+    Serial.println("\n\nShift detected. Blue parameter ready.");
     blue = inString.toInt();
-    analogWrite(11,255-red);
+    Serial.println("\n\nShift detected. Writing on its way.");
+    analogWrite(11,255-blue);
     analogWrite(9,255-green);
-    analogWrite(10,255-blue);
+    analogWrite(10,255-red);
 
     Serial.print("Red: ");
     Serial.print(red);
@@ -66,10 +95,11 @@ void setup() {
 
     currentColor = 0;
   }*/
-
-void loop(){
-    analogWrite(11,255);
-    analogWrite(9,0);//R
-    analogWrite(10,0);
-
 }
+
+/*void loop(){
+    analogWrite(11,255);//B
+    analogWrite(9,0);//R
+    analogWrite(10,0);//G
+
+}*/
