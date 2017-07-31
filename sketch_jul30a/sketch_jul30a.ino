e963d089eec5e441ee87545b8cdc89757413f5cd
dyingcarl <carluner@gmail.com>
2017-07-31 13:24:19 +0800

    PhysicalSwitchingAdded
    Now it is possible to use another analog input to change the color of
    LED bubble. There is also a mode to let the LED change its color on its
    onw based on turns.

--- sketch_jul30a/sketch_jul30a.ino
+++ sketch_jul30a/sketch_jul30a.ino
@@ -1,6 +1,8 @@
 String inString ="";
 int currentColor = 0;
 int red,green,blue = 0;
+int reading = 0;
+int rainbowFlag = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -18,30 +20,51 @@ void setup() {
   pinMode(13,OUTPUT);
   digitalWrite(13,HIGH);
 
+  pinMode(8,INPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  
   int inChar;
 
   if(Serial.available()>0){
     inChar = Serial.read();
-    Serial.println("\n\nsSerial availability verified.");
+    Serial.println("\nSerial availability verified.");
   }
 
   if(isDigit(inChar)){
     inString += (char)inChar;
-    Serial.println("\n\nInput detected, inString has been changed.");
+    Serial.println("\nInput detected, inString has been changed.");
+  }
+  if(inChar == 'a'){
+    rainbowFlag++;
+    
+    Serial.println("\nrainbowFlag increased by 1.");
+  }
+  if(rainbowFlag%2 ==1){
+    //reading = reading + 10;
+    //if(reading > 1030){
+    //  reading = 0;
+   // }
+    reading = analogRead(A0);
+    Serial.println("\nanalogReading is loaded.");
+    //setRGB(map(reading,300,500,0,1023));
+    setRGB(reading);
+    Serial.println("\nRGB set.");
+    Serial.print(reading);
+    delay(350);
   }
 
   if(inChar == ','){
-    Serial.println("\n\nSwitch on the way.");
+    Serial.println("\nSwitch on the way.");
     switch(currentColor){
       case 0:
         red = inString.toInt();
         inString = "";
-        Serial.println("\n\nRed parameter ready, inString cleared.");
+        Serial.println("\nRed parameter ready, inString cleared.");
         analogWrite(10,255-red);
         Serial.print(red);
         Serial.print(green);
@@ -51,7 +74,7 @@ void loop() {
       case 1:
         green = inString.toInt();
         inString = "";
-        Serial.println("\n\nGreen parameter ready, inString cleared.");
+        Serial.println("\nGreen parameter ready, inString cleared.");
         analogWrite(9,255-green);
         Serial.print(red);
         Serial.print(green);
@@ -61,7 +84,7 @@ void loop() {
       case 2:
         blue = inString.toInt();
         inString = "";
-        Serial.println("\n\nBlue parameter ready, inString cleared.");
+        Serial.println("\nBlue parameter ready, inString cleared.");
         analogWrite(11,255-blue);
         Serial.print(red);
         Serial.print(green);
@@ -95,8 +118,67 @@ void loop() {
 
     currentColor = 0;
   }*/
+
+  
 }
 
+void setR(int r){
+  if(r < 337){
+    red = 255;
+  }
+  else if(r > 486){
+    red = 0;
+  }
+  else{
+    red = 255-map(r,337,486,0,255);
+  }
+  analogWrite(10,red);
+  return;
+}
+
+void setG(int g){
+  if((g < 637) &&(g > 186)){
+    green = 0;
+  }
+  else if(g > 786){
+    green = 255;
+  }
+  else if(g < 187){
+    green = 255-map(g,37,186,0,255);
+  }
+  else if((g > 636)&&(g < 787)){
+    green = 0+map(g,637,786,0,255);
+  }
+  analogWrite(9,green);
+  return;
+}
+
+void setB(int b){
+  if((b < 187)||((b > 636) &&(b < 787))){
+    blue = 255;
+  }
+  else if((b > 487) &&(b < 336)){
+    blue = 0;
+  }
+  else if((b > 186)&&(b < 337)){
+    blue = 255-map(b,187,336,0,255);
+  }
+  else if((b > 786)&&(b < 937)){
+    blue = 255-map(b,787,936,0,255);
+  }
+  else if((b > 487)&&(b < 636)){
+    blue = 0+map(b,487,636,0,255);
+  }
+  analogWrite(11,blue);
+  return;
+}
+
+int setRGB(int num){
+  setR(num);
+  setG(num);
+  setB(num);
+  return 1;
+}
 /*void loop(){
     analogWrite(11,255);//B
     analogWrite(9,0);//R
